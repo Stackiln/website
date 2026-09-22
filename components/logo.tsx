b913b59356f120cef9722a05891eccb20975e522
Christian Relf <christianjamesrelf@gmail.com>
2026-09-22 02:33:23 +0100

    brand: use the official Stackiln mark throughout

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,18 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Mark({ size = 36 }: { size?: number }) {
   return (
-    <svg
-      aria-hidden="true"
+    <Image
+      alt=""
       className="mark"
       height={size}
-      viewBox="0 0 44 44"
+      priority
+      src="/stackiln-mark.png"
       width={size}
-    >
-      <path d="M5 6h27l7 6-7 6H5l7-6-7-6Z" fill="#ff9f1c" />
-      <path d="M5 18h27l7 6-7 6H5l7-6-7-6Z" fill="#ff6436" />
-      <path d="M5 30h27l7 6-7 6H5l7-6-7-6Z" fill="#e93f2c" />
-    </svg>
+    />
   );
 }
 
